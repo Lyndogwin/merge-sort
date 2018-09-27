774d4e03e2616613da76f1103f45eff4edee26a8
Lyndogwin <brand949@gmail.com>
2018-09-27 15:01:44 -0400

    Added binary search function

--- PlusUltra.java
+++ PlusUltra.java
@@ -2,30 +2,46 @@ import java.util.*;
 public class PlusUltra{
 	public static int mCounter=0;
 	public static void main(String[] args) {
-		//get array from input
-		Scanner keyb=new Scanner(System.in);
-		System.out.print("Enter an array separated with element separated by commas: ");
-		String input=keyb.next();
-		System.out.println();
+		Scanner keyb = new Scanner(System.in);
+		boolean arrayIn = false;
+		boolean exit = false;
+		String ans = "0";
+		int key = -1;
+		ArrayList<Integer> sorted = new ArrayList<Integer>();
 
-		//spit input into array
-		String parts[] = input.split(",");
-		//initialize an integer array that is the same length as the inputed array
-		int numbers[] = new int[parts.length];
-
-		//parse each element of "parts" to int value and set elements of "numbers" to those values at the corresponding indexes 
-		//print each value of numbers to test
-		for(int i=0;i<parts.length;i++){
-			numbers[i]=Integer.parseInt(parts[i]);
-			//System.out.print(numbers[i]+",");
-		}
-
-		//run numbers through the merge sort
-		mergeSort(numbers,0,numbers.length-1);
-
-		//print the sorted array
-		for(int i=0;i<numbers.length;i++){
-			System.out.print(numbers[i]+",");
+		while(!exit){
+			System.out.println("Please make a menu selection: \n"+
+				"1: Sort an array from input.\n"+
+				"2: Search recently sorted array for value.\n"+
+				"3: Exit.\n");
+			ans = keyb.next();
+			
+			if(ans.equals("1")){
+				arrayIn = false;
+				int num[] = inputArray(keyb);
+				System.out.println("Array length is "+num.length+
+					"\nWith last index at "+(num.length - 1));
+				for(int i=0;i<num.length;i++){
+					sorted.add(num[i]);
+					System.out.print(sorted.get(i)+",");
+				}
+				System.out.println();
+				arrayIn = true;
+			}
+			else if(ans.equals("2")){
+				if (arrayIn){
+					System.out.println("What is the number you're looking for? ");
+					key = keyb.nextInt();
+					recursiveBinarySearch(sorted,key, 0,(sorted.size()-1));
+					//binarySearch(sorted,keyb);
+				}
+				else{
+					System.out.println("Please use option one to create and sort an array first.\n");
+				}
+			}
+			else if(ans.equals("3")){
+				exit = true;
+			}
 		}
 	}
 
@@ -88,11 +104,86 @@ public class PlusUltra{
 		 // Recursively sort left and right partitions
 		 mergeSort(numbers, i, j);
 		 mergeSort(numbers, j + 1, k);
+		 System.out.println("Split complete(mergeSort) From:"+numbers[i]+" to "+numbers[k]);
 
-		 // Merge left and right partition in sorted order
-		 // ------does this run asynchronously while mergeSort() is recursively called--???????????????  
+		 // Merge left and right partition in sorted order  
 		 merge(numbers, i, j, k);
 		}
 	}
 
+
+	public static void binarySearch(ArrayList<Integer> numbers, Scanner keyb){
+		int i = 0;
+		int k = numbers.size() - 1;
+		int j = 0;
+		boolean found = false;
+
+		System.out.println("What is the number you're looking for? ");
+		int search = keyb.nextInt();
+
+		while (i <= k && !found){
+			System.out.println("Current mid point: "+j);
+			System.out.println("searching...");
+			j = (i + k) / 2;
+			if(numbers.get(j) < search){
+				i = j + 1;
+			}
+			else if(numbers.get(j) > search){
+				k = j - 1;
+			}
+			else{
+				System.out.println("Value found at index "+j);
+				found = true;
+			}
+		}
+	}
+
+	// the redundance is for academic analysis
+	public static void recursiveBinarySearch(ArrayList<Integer> numbers, int key, int i, int k){
+		System.out.println("searching...");
+		int j = (i + k) / 2;
+
+		if (numbers.get(j) == key){
+			// if midpoint "j" is equal to key, recursion will cease 
+			System.out.println("Value found at index "+j);
+		}
+		else if(i != k){
+			//recusively split array until match is found
+			recursiveBinarySearch(numbers,key,(j + 1),k);
+			recursiveBinarySearch(numbers,key,i,(j - 1));
+		}
+		else{
+			System.out.println("Value is not in array.");
+		}
+
+	}
+
+	public static int[] inputArray(Scanner keyb){
+		//get array from input
+		System.out.print("Enter an array separated with element separated by commas: ");
+		String input=keyb.next();
+		System.out.println();
+
+		//spit input into array
+		String parts[] = input.split(",");
+		//initialize an integer array that is the same length as the inputed array
+		int numbers[] = new int[parts.length];
+
+		//parse each element of "parts" to int value and set elements of "numbers" to those values at the corresponding indexes 
+		//print each value of numbers to test
+		for(int i = 0;i < parts.length;i++){
+			numbers[i] = Integer.parseInt(parts[i]);
+			//System.out.print(numbers[i]+",");
+		}
+
+		//run numbers through the merge sort
+		mergeSort(numbers,0,numbers.length-1);
+
+		//print the sorted array
+		for(int i=0;i<numbers.length;i++){
+			//System.out.print(numbers[i]+",");
+		}
+		System.out.println();
+		return numbers;
+	}
 }
